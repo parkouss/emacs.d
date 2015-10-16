8d1d4b8abd9e14c5a790188246f67f8272c0da06
Julien Pagès <j.parkouss@gmail.com>
2015-10-16 19:52:37 +0200

    Disable js trailing comma warnings

--- modules/weemacs-javascript.el
+++ modules/weemacs-javascript.el
@@ -10,6 +10,8 @@
 (add-to-list 'auto-mode-alist '("\\.js\\'" . js2-mode))
 
 (setq-default js2-basic-offset 2)
+;; no warning for trailing commas
+(setq-default js2-strict-trailing-comma-warning nil)
 
 (add-hook 'js2-mode-hook (lambda ()
                            (tern-mode t)
