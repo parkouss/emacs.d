6f7f7c61e8b6bf527313535ad487ac14bdb28b4c
Julien Pagès <j.parkouss@gmail.com>
2015-10-18 11:48:58 +0200

    add pytest support

--- modules/weemacs-python.el
+++ modules/weemacs-python.el
@@ -3,6 +3,18 @@
 (weemacs-require-package 'anaconda-mode)
 (weemacs-require-package 'company-anaconda)
 (weemacs-require-package 'pyvenv)
+(weemacs-require-package 'pytest)
+
+(require 'pytest)
+(add-hook 'python-mode-hook
+          (lambda ()
+            (local-set-key "\C-ca" 'pytest-all)
+            (local-set-key "\C-cm" 'pytest-module)
+            (local-set-key "\C-c." 'pytest-one)
+            (local-set-key "\C-cd" 'pytest-directory)
+            (local-set-key "\C-cpa" 'pytest-pdb-all)
+            (local-set-key "\C-cpm" 'pytest-pdb-module)
+            (local-set-key "\C-cp." 'pytest-pdb-one)))
 
 ;; first Add company-anaconda to allowed company-mode backends list
 (eval-after-load "company"
