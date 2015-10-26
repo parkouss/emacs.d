a78a7a58ce388c4cf65ed82222e2d33861cc1081
Julien Pagès <j.parkouss@gmail.com>
2015-10-26 06:42:13 +0100

    Add dot and html output coloring

--- modules/weemacs-org.el
+++ modules/weemacs-org.el
@@ -1,6 +1,8 @@
 (weemacs-require-package 'org)
 (weemacs-require-package 'ox-reveal)
+(weemacs-require-package 'htmlize) ;; code coloring for org-reveal
 
 (require 'ox-reveal)
+(require 'ob-dot)
 
 (provide 'weemacs-org)
