01413a20f14eff5923291e184b8dbfd88efe2758
Julien Pagès <j.parkouss@gmail.com>
2016-01-26 12:20:31 +0100

    Allow windmove to work in org mode

--- modules/weemacs-org.el
+++ modules/weemacs-org.el
@@ -5,4 +5,10 @@
 (require 'ox-reveal)
 (require 'ob-dot)
 
+;; Make windmove work in org-mode:
+(add-hook 'org-shiftup-final-hook 'windmove-up)
+(add-hook 'org-shiftleft-final-hook 'windmove-left)
+(add-hook 'org-shiftdown-final-hook 'windmove-down)
+(add-hook 'org-shiftright-final-hook 'windmove-right)
+
 (provide 'weemacs-org)
