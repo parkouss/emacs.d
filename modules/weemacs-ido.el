dd8846980c1d7df5edd30fa6f7cdd95f528ce4ad
Julien Pagès <j.parkouss@gmail.com>
2016-02-29 20:41:30 +0100

    remove ido smart open file

--- modules/weemacs-ido.el
+++ modules/weemacs-ido.el
@@ -8,7 +8,6 @@
 (setq ido-enable-prefix nil
       ido-enable-flex-matching t
       ido-create-new-buffer 'always
-      ido-use-filename-at-point 'guess
       ido-max-prospects 10
       ido-save-directory-list-file (expand-file-name "ido.hist" weemacs-savefile-dir)
       ido-default-file-method 'selected-window
