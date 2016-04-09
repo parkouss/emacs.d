e74cd1c634b970792418619ed7c67b357732fd30
Julien Pagès <j.parkouss@gmail.com>
2016-04-09 11:40:02 +0200

    don't allow jabber to spam the mode line

--- modules/weemacs-jabber.el
+++ modules/weemacs-jabber.el
@@ -4,4 +4,12 @@
                             ;; steery.io account
                             ("213996_3664926@chat.hipchat.com")))
 
+(eval-after-load 'jabber
+  `(progn
+     ;; Message alert hooks
+     (define-jabber-alert echo "Show a message in the echo area"
+       (lambda (msg)
+         (unless (minibuffer-prompt)
+           (message "%s" msg))))))
+
 (provide 'weemacs-jabber)
