aebb5599c8fcfcfd972fb39308e8cd6a65e34525
Julien Pagès <j.parkouss@gmail.com>
2016-06-18 11:18:18 +0200

    allow to disable some modules

--- init.el
+++ init.el
@@ -3,6 +3,18 @@
 
 (require 'org)
 
+;; allow to store modules I want to disable
+;; example: (setq modules-disabled '((pdf . t)))
+(setq modules-disabled '())
+
+;; allow to override, based on some config
+(let ((conf-file (expand-file-name "custom.el" my-conf-dir)))
+  (if (file-exists-p conf-file)
+      (load conf-file)))
+
+(defun module-disabled? (module)
+  (cdr (assoc module modules-disabled)))
+
 ;; secrets
 (load (expand-file-name "passwd.el"
                         (expand-file-name "private" my-conf-dir)))
